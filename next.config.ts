8ca3fd7bf31222f3f911563e7701ffd4c3d06d64
hydrotechvn <hydrotechvn@gmail.com>
2026-02-04 15:53:24 +0700

    Allow picsum.photos for mock product images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+      },
     ],
     formats: ['image/webp', 'image/avif'],
   },
